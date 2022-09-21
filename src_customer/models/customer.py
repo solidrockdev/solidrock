fb804f3cef1343b076b138a48d0898f9e4a112bb
Vaishnav <vaishnav@ontash.net>
2022-09-21 14:50:56 +0530

    added inverntory and field service

---
+++ src_customer/models/customer.py
@@ -0,0 +1,62 @@
+from odoo import fields, models, api
+
+
+class Customer(models.Model):
+    '''
+        Inherit partner module to create a new customized customer module
+    '''
+    _name = 'my.customer'
+    _inherits = {'res.partner': 'partner_id'}
+
+    customer_type = fields.Many2one('my.customer.type',string="Customer Type")
+    first_name = fields.Char(string="First Name")
+    middle_initial = fields.Char(string="Middle Initial")
+    last_name = fields.Char(string="Last Name")
+    primary_contact = fields.Char(string="Primary Contact")
+    secondary_contact = fields.Char(string="Secondary Contact")
+    balance = fields.Float()
+    balance_total = fields.Float()
+    terms_id = fields.Many2one('account.payment.term', string="Terms")
+    tax_code = fields.Integer(string="Tax Code")
+    tax_item = fields.Many2one('account.tax', string="Tax Item")
+    rep_id = fields.Many2one('hr.employee', string="Rep")
+    resale = fields.Char()
+    account_no = fields.Char()
+    job_status = fields.Char()
+    job_type = fields.Selection([('commercial', 'Commercial'), ('residential', 'Residential')], string="Job Type")
+    job_description = fields.Text()
+    start_date = fields.Date()
+    projected_end = fields.Date()
+    end_date = fields.Date()
+    street_ship = fields.Char()
+    street2_ship = fields.Char()
+    zip_ship = fields.Char(change_default=True)
+    city_ship = fields.Char()
+    state_id_ship = fields.Many2one("res.country.state", string='State', ondelete='restrict',
+                               domain="[('country_id', '=?', country_id)]")
+    country_id_ship = fields.Many2one('res.country', string='Country', ondelete='restrict')
+    country_code_ship = fields.Char(related='country_id.code', string="Country Code")
+
+    # fields for smart buttons
+    sale_order_count = fields.Integer()
+    purchase_order_count = fields.Integer()
+    total_invoiced = fields.Integer()
+    currency_id = fields.Integer()
+    open_partner_ledger = fields.Integer()
+    supplier_invoice_count = fields.Integer()
+
+
+    # def action_open_employees(self):
+    #     return
+    # def sale_order_count(self):
+    #     return
+
+
+
+
+class CustomerType(models.Model):
+    '''
+        Inherit partner module to create a new customized customer module
+    '''
+    _name = 'my.customer.type'
+
---
+++ src_inventory/models/inventory_main.py
@@ -0,0 +1,19 @@
+from odoo import models, fields, api
+
+
+class Inventorys(models.Model):
+    '''
+           Inherit product.template model to create a new customized inventory module
+    '''
+
+    _inherit = "product.template"
+
+    warranty_start_date = fields.Date(string="Warranty Start Date")
+    warranty_end_date = fields.Date(string="Warranty End Date")
+    expiration_date = fields.Date(string="Expiration Date")
+    is_farm_equipment_base = fields.Boolean(string="Is Farm Equipment Base", default=False)
+
+
+
+
+
