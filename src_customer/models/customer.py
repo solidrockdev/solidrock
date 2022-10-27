bfb07db68bc5931ab6163c0ea771af05da611284
Vaishnav <vaishnav@ontash.net>
2022-10-27 19:35:35 +0530

    Added customer, sales, vendor and field service

---
+++ src_customer/models/customer.py
@@ -0,0 +1,315 @@
+from odoo import fields, models, api
+
+
+# class Customer(models.Model):
+#     '''
+#         Inherit partner module to create a new customized customer module
+#     '''
+#     _name = 'my.customer'
+#     _inherits = {'res.partner': 'partner_id'}
+
+    # customer_type = fields.Many2one('my.customer.type',string="Customer Type")
+    # company = fields.Char(string="Company")
+    # first_name = fields.Char(string="First Name")
+    # middle_initial = fields.Char(string="Middle Initial")
+    # last_name = fields.Char(string="Last Name")
+    # primary_contact = fields.Char(string="Primary Contact")
+    # secondary_contact = fields.Char(string="Secondary Contact")
+    # balance = fields.Float()
+    # balance_total = fields.Float()
+    # terms_id = fields.Many2one('account.payment.term', string="Terms")
+    # tax_code = fields.Char(string="Tax Code")
+    # tax_item = fields.Many2one('account.tax', string="Tax Item")
+    # rep_id = fields.Many2one('hr.employee', string="Rep")
+    # resale = fields.Char()
+    # account_no = fields.Char()
+    # job_status = fields.Char()
+    # job_type = fields.Selection([('commercial', 'Commercial'), ('residential', 'Residential')], string="Job Type")
+    # job_description = fields.Text()
+    # start_date = fields.Date()
+    # projected_end = fields.Date()
+    # end_date = fields.Date()
+    # street_ship = fields.Char()
+    # street2_ship = fields.Char()
+    # zip_ship = fields.Char(change_default=True)
+    # city_ship = fields.Char()
+    # state_id_ship = fields.Many2one("res.country.state", string='State', ondelete='restrict',
+    #                            domain="[('country_id', '=?', country_id)]")
+    # country_id_ship = fields.Many2one('res.country', string='Country', ondelete='restrict')
+    # country_code_ship = fields.Char(related='country_id.code', string="Country Code")
+    # fax = fields.Char()
+
+    # fields for smart buttons
+    # sale_order_count = fields.Integer()
+    # purchase_order_count = fields.Integer()
+    # total_invoiced = fields.Integer()
+    # currency_id = fields.Integer()
+    # open_partner_ledger = fields.Integer()
+    # supplier_invoice_count = fields.Integer()
+    # task_count = fields.Integer()
+
+
+
+
+class PartnerInherit(models.Model):
+    '''
+           Inherit res.Partner model to create a new customized customer module
+    '''
+
+    _inherit = "res.partner"
+
+    company_type = fields.Selection(string='Company Type', selection=[('person', 'Individual'), ('company', 'Company')],
+                                    compute='_compute_company_type', inverse='_write_company_type', default='person')
+    customer_type = fields.Many2one('my.customer.type',string="Customer Type")
+    vendor_type = fields.Many2one('my.vendor.type',string="Vendor Type")
+    first_name = fields.Char(string="First Name")
+    middle_initial = fields.Char(string="Middle Initial")
+    last_name = fields.Char(string="Last Name")
+    balance = fields.Float()
+    balance_total = fields.Float()
+    terms_id = fields.Many2one('account.payment.term', string="Terms")
+    tax_code = fields.Char(string="Tax Code")
+    tax_item = fields.Many2one('account.tax', string="Tax ID")
+    rep_id = fields.Many2one('hr.employee', string="Rep")
+    street_ship = fields.Char()
+    street2_ship = fields.Char()
+    street3 = fields.Char()
+    street3_ship = fields.Char()
+    zip_ship = fields.Char(change_default=True)
+    city_ship = fields.Char()
+    state_id_ship = fields.Many2one("res.country.state", string='State', ondelete='restrict', domain="[('country_id', '=?', country_id)]")
+    country_id_ship = fields.Many2one('res.country', string='Country', ondelete='restrict')
+    resale = fields.Char()
+    account_no = fields.Char()
+    job_status = fields.Char()
+    job_type = fields.Selection([('commercial', 'Commercial'), ('residential', 'Residential')], string="Job Type")
+    job_description = fields.Text()
+    start_date = fields.Date()
+    projected_end = fields.Date()
+    end_date = fields.Date()
+    product_detail_ids = fields.One2many('product.info', 'partner_id', string='Stock')
+    # supplier_rank = fields.Integer(default=0, copy=False)
+    # customer_rank = fields.Integer(default=0, copy=False)
+    customer_base_discount = fields.Float()
+    customer_early_order_discount = fields.Float()
+    customer_early_pay_discount = fields.Float()
+    early_order_deadline = fields.Date()
+
+    primary_contact = fields.Char(string="Primary Contact")
+    fax = fields.Char(string="Fax")
+    work_phone = fields.Char(string="Work Phone")
+    home_phone = fields.Char(string="Home Phone")
+    alt_phone = fields.Char(string="Alt. Phone")
+    alt_mobile = fields.Char(string="Alt. Mobile")
+    linkedIn = fields.Char(string="LinkedIn")
+    facebook = fields.Char(string="Facebook")
+    twitter = fields.Char(string="Twitter")
+    alt_email1 = fields.Char(string="Alt. Email 1")
+    alt_email2 = fields.Char(string="Alt. Email 2")
+    cc_email = fields.Char(string="CC Email")
+    date_added = fields.Date(string="Date Added", default=fields.Date.context_today)
+    from_timer = fields.Selection([('yes', 'Yes'), ('no', 'No')], string="From Timer")
+    attach = fields.Selection([('yes', 'Yes'), ('no', 'No')], string="Attach")
+    eligible_for_1099 = fields.Selection([('yes', 'Yes'), ('no', 'No')], string="Eligible For 1099")
+    print_on_check_as = fields.Char(string="Print on Cheque As")
+    notes = fields.Selection([('has_notes', 'Has Notes'), ('no_notes', 'No Notes')], string="Notes", default="has_notes")
+    role = fields.Char(string="Role")
+    ''' 
+        Added customer and vendor radio buttons
+    '''
+    is_customer_vendor = fields.Selection(string='Contact Type', selection=[('is_customer', 'Customer'), ('is_vendor', 'Vendor')], default='is_customer')
+    is_date_based_disc = fields.Selection(selection=[('yes', 'Yes'), ('no', 'No')], string="Date Based Discounts", default='no')
+    is_vol_based_disc = fields.Selection(selection=[('yes', 'Yes'), ('no', 'No')], string="Volume Based Discounts", default='no')
+    is_cus_based_disc = fields.Selection(selection=[('yes', 'Yes'), ('no', 'No')], string="Customer Discounts", default='no')
+
+
+    db_std_base_dealer_disc = fields.Float(string="Standard Base Dealer Discount")
+    db_level_2_disc = fields.Float(string="Level 2 Discount")
+    db_level_3_dealer_disc = fields.Float(string="Level 3 Dealer Discount")
+    db_free_freight_vol = fields.Float(string="Free Freight Volume")
+    db_free_freight_date_range_start = fields.Date(string="Free Freight Start Date")
+    db_free_freight_date_range_end = fields.Date(string="Free Freight End Date")
+    additional_notes = fields.Text(string="Additional Notes")
+
+
+    vb_volume_disc_level_1 = fields.Float(string="Volume Discount Level 1")
+    vb_level_1_disc = fields.Float(string="Level 1 Discount")
+    vb_volume_disc_level_2 = fields.Float(string="Volume Discount Level 2")
+    vb_level_2_disc = fields.Float(string="Level 2 Discount")
+    vb_volume_disc_level_3 = fields.Float(string="Volume Discount Level 3")
+    vb_level_3_disc = fields.Float(string="Level 3 Discount")
+    vb_volume_disc_level_4 = fields.Float(string="Volume Discount Level 4")
+    vb_level_4_disc = fields.Float(string="Level 4 Discount")
+
+    '''for contact 2'''
+    contact_name = fields.Many2one('res.partner',string="Contact")
+    first_name1 = fields.Char(string="First Name")
+    middle_initial1 = fields.Char(string="Middle Initial")
+    last_name1 = fields.Char(string="Last Name")
+    role1 = fields.Char(string="Role")
+    job_title = fields.Char(string="Job Title")
+    mobile1 = fields.Char(string="Mobile")
+    work_phone1 = fields.Char(string="Work Phone")
+    home_phone1 = fields.Char(string="Home Phone")
+    email1 = fields.Char(string="Email")
+    street_contact2 = fields.Char()
+    street2_contact2 = fields.Char()
+    zip_contact2 = fields.Char(change_default=True)
+    city_contact2 = fields.Char()
+    state_id_contact2 = fields.Many2one("res.country.state", string='State', ondelete='restrict', domain="[('country_id', '=?', country_id)]")
+    country_id_contact2 = fields.Many2one('res.country', string='Country', ondelete='restrict')
+
+
+
+    @api.onchange('contact_name')
+    def onchange_contact(self):
+        if self.contact_name:
+            print(self.contact_name)
+            contact = self.env['res.partner'].search([('id', '=', self.contact_name.id)])
+            print(contact)
+            print(contact.first_name)
+            self.first_name1 = contact.first_name
+            self.middle_initial1 = contact.middle_initial
+            self.last_name1 = contact.last_name
+            self.role1 = contact.role
+            self.job_title = contact.function
+            self.mobile1 = contact.mobile
+            self.work_phone1 = contact.work_phone
+            self.home_phone1 = contact.home_phone
+            self.email1 = contact.email
+            self.street_contact2 = contact.street
+            self.street2_contact2 = contact.street2
+            self.zip_contact2 = contact.zip
+            self.city_contact2 = contact.city
+            self.state_id_contact2 = contact.state_id
+            self.country_id_contact2 = contact.country_id
+
+
+
+
+    @api.onchange('is_customer_vendor')
+    def onchange_customer(self):
+        if self.is_customer_vendor == 'is_customer':
+            self.supplier_rank = 0
+            self.customer_rank = 1
+        else:
+            self.supplier_rank = 1
+            self.customer_rank = 0
+
+
+
+''' 
+    
+    Farm Equipment Base model
+'''
+
+
+# Farm Equipment Base model
+
+class ProductDetails(models.Model):
+    _name = 'product.info'
+
+    name_id = fields.Many2one('product.template',ondelete='restrict', index=True,)
+    product_category_id = fields.Many2one('product.category', ondelete='restrict', index=True, )
+    warranty_start_date = fields.Date(string="Start Date")
+    warranty_end_date = fields.Date(string="Warranty End Date")
+    expiration_date = fields.Date(string="Expiration Date")
+    # product_stock_id = fields.Many2one('my.customer')
+    partner_id = fields.Many2one('res.partner')
+    company_id = fields.Many2one('res.company', 'Company', index=1)
+    model = fields.Char(string="Model")
+    year = fields.Char(string="Year")
+    serial_no = fields.Many2many('stock.production.lot', 'customer_product_serial_no', string='Serial No')
+    # product_stocks_ids = fields.Many2one('my.customer')
+
+    @api.onchange('name_id')
+    def onchange_skus(self):
+        for record in self:
+            # print(record.id)
+            skus = self.env['product.template'].search(
+                [('id', '=', record.name_id.id), ('name', '=', record.name_id.name)], limit=1)
+            record.product_category_id = skus.categ_id.id
+            record.warranty_start_date = skus.warranty_start_date
+            record.warranty_end_date = skus.warranty_end_date
+            record.expiration_date = skus.expiration_date
+            record.company_id = skus.company_id
+            record.model = skus.model
+            record.year = skus.year
+            record.partner_id = record.id
+            record.serial_no = skus.serial_no
+
+
+
+
+
+
+
+
+
+class SkuDetails(models.Model):
+    _name = 'sku.details'
+
+    name = fields.Char()
+    warranty_start_date = fields.Date(string="Start Date")
+
+
+
+
+
+class CustomerType(models.Model):
+    '''
+        Inherit partner module to create a new customized customer module
+    '''
+    _name = 'my.customer.type'
+    name = fields.Char()
+
+
+
+
+class VendorType(models.Model):
+    '''
+        Inherit partner module to create a new customized customer module
+    '''
+    _name = 'my.vendor.type'
+    name = fields.Char()
+
+
+class InvoiceInherit(models.Model):
+    '''
+           Inherit account.Move model
+    '''
+
+    _inherit = "account.move"
+
+    estimate = fields.Monetary(string="Estimate")
+
+
+class InvoiceInheritLine(models.Model):
+    '''
+           Inherit account.Move.line model
+    '''
+
+    _inherit = "account.move.line"
+
+    so_no = fields.Char(string="S.O.No.")
+
+class PurchaseorderInherit(models.Model):
+        '''
+               Inherit purchase.order model
+        '''
+
+        _inherit = "purchase.order"
+
+        customer = fields.Char(string="Customer")
+
+class PurchaseOrderLineInherit(models.Model):
+    '''
+           Inherit Purchase.Order.line model
+    '''
+
+    _inherit = "purchase.order.line"
+
+    mpn = fields.Char(string="MPN")
+
+
+
