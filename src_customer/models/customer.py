603dccd15279dbf80cd92291c50eb011b60e5080
solidrockdev <solidrockops@ontash.net>
2022-11-10 18:17:30 +0530

    From_Jenkins_2022.11.10-18.17.30

--- src_customer/models/customer.py
+++ src_customer/models/customer.py
@@ -129,7 +129,66 @@ class PartnerInherit(models.Model):
     end_date = fields.Date()
 
     '''Added for Farm Equipment tab'''
-    product_detail_ids = fields.One2many('product.info', 'partner_id', string='Stock')
+
+    company_id = fields.Many2one('res.company', 'Company', index=1, readonly=False)
+    # partner_id = fields.Many2one('res.partner', readonly=False)
+    warranty_start_date = fields.Date(string="Warranty Start Date", readonly=False)
+    warranty_end_date = fields.Date(string="Warranty End Date", readonly=False)
+    expiration_date = fields.Date(string="Expiration Date", readonly=False)
+    is_farm_equipment_base = fields.Boolean(string="Is Farm Equipment Base", default=False, readonly=False)
+    customer = fields.Many2one('res.partner', string="Customer", readonly=False)
+    model = fields.Char(string="Model", readonly=False)
+    year = fields.Char(string="Year", readonly=False)
+    rows = fields.Integer(string="Rows", readonly=False)
+    crops_being_planted = fields.Char(string="Crops being planted", readonly=False)  # many2many
+    acres_planted_per_year = fields.Char(string="Acres Planted Per Year", readonly=False)
+    spacing = fields.Integer(string="Spacing", readonly=False)
+    frame_type = fields.Char(string="Frame Type", readonly=False)  # many2one
+    seed_delivery = fields.Char(string="Seed Delivery", readonly=False)
+    original_planter_monitor = fields.Char(string="Original Planter Monitor", readonly=False)
+    current_planter_monitor = fields.Char(string="Current Planter Monitor", readonly=False)
+    meter_type = fields.Char(string="Meter Type", readonly=False)  # many2one
+    meter_drive_system = fields.Char(string="Meter Drive System", readonly=False)  # many2one
+    closing_system = fields.Char(string="Closing System", readonly=False)  # many2one
+    hopper_type = fields.Char(string="Hopper Type", readonly=False)  # many2one
+    row_config = fields.Char(string="Row Configuration", readonly=False)
+    no_of_regular_parallel_arms = fields.Integer(string="No of Regular Parallel Arms", readonly=False)
+    no_of_long_parallel_arms = fields.Integer(string="No of Long Parallel Arms", readonly=False)
+    downforce_system = fields.Char(string="Down Force System", readonly=False)
+    no_of_vr_motors = fields.Integer(string="No of VR Motors", readonly=False)
+    row_cleaner_make = fields.Char(string="Row Cleaner Make", readonly=False)
+    row_cleaner_model = fields.Char(string="Row Cleaner Model", readonly=False)
+    no_till_coulters = fields.Char(string="No-Till Coulters", readonly=False)
+    liquid_application_method_1 = fields.Char(string="Liquid Application Method #1", readonly=False)  # many2one
+    liquid_application_method_2 = fields.Char(string="Liquid Application Method #2", readonly=False)  # many2one
+    firmer = fields.Char(string="Firmer", readonly=False)
+    gps_make = fields.Char(string="GPS Make", readonly=False)
+    gps_monitor = fields.Char(string="GPS Monitor", readonly=False)
+    no_of_hydraulic_remotes = fields.Integer(string="No of Hydraulic Remotes", readonly=False)
+    hydraulic_capacity = fields.Char(string="Hydraulic Capacity", readonly=False)
+    width = fields.Float(string="Width", readonly=False)
+    monitor = fields.Char(string="Monitor", readonly=False)
+    mpn = fields.Char(string="MPN")
+    tax_agency = fields.Char(string="Tax Agency")
+    assets_account = fields.Many2one('account.account', string='Assets Account')
+    accumulated_depreciation = fields.Float(string='Accumulated Depreciation')
+    # serial_no = fields.Many2many('stock.production.lot', 'contact_product_serial_no', string='Serial No')
+    preferred_vendor = fields.Many2one('res.partner', string="Preferred Vendor")
+    # reorder_pt_min = fields.Float(string='Reorder Pt (Min)')
+    # reorder_pt_max = fields.Float(string='Max')
+    tax_status = fields.Selection([
+        ('tax', 'Tax'),
+        ('non', 'Non'),
+        ('yes', 'Yes'),
+    ], default='non', string="Tax Status")
+    serial_no = fields.Many2many('stock.production.lot', string='Serial No')
+
+    # categ_id = fields.Many2one(
+    #     'product.category', 'Product Category', ondelete='cascade',
+    #     help="Specify a product category if this rule only applies to products belonging to this category or its children categories. Keep empty otherwise.")
+
+    # categ_id = fields.Selection([('planter', 'Planter'), ('tractor', 'Tractor'), ('combine', 'Combine'), ('combinehead', 'Combine Head'), ('sprayer', 'Sprayer')], string="Product Line")
+
 
     '''Added fields for customer discounts tab'''
 
@@ -302,34 +361,76 @@ class PartnerInherit(models.Model):
 class ProductDetails(models.Model):
     _name = 'product.info'
 
-    name_id = fields.Many2one('product.template',ondelete='restrict', index=True,)
-    product_category_id = fields.Many2one('product.category', ondelete='restrict', index=True, )
-    warranty_start_date = fields.Date(string="Start Date")
-    warranty_end_date = fields.Date(string="Warranty End Date")
-    expiration_date = fields.Date(string="Expiration Date")
-    # product_stock_id = fields.Many2one('my.customer')
-    partner_id = fields.Many2one('res.partner')
-    company_id = fields.Many2one('res.company', 'Company', index=1)
-    model = fields.Char(string="Model")
-    year = fields.Char(string="Year")
-    serial_no = fields.Many2many('stock.production.lot', 'customer_product_serial_no', string='Serial No')
-    # product_stocks_ids = fields.Many2one('my.customer')
+    company_id = fields.Many2one('res.company', 'Company', index=1, readonly=False)
+    partner_id = fields.Many2one('res.partner', readonly=False)
+    warranty_start_date = fields.Date(string="Warranty Start Date", readonly=False)
+    warranty_end_date = fields.Date(string="Warranty End Date", readonly=False)
+    expiration_date = fields.Date(string="Expiration Date", readonly=False)
+    is_farm_equipment_base = fields.Boolean(string="Is Farm Equipment Base", default=False, readonly=False)
+    customer = fields.Many2one('res.partner', string="Customer", readonly=False)
+    model = fields.Char(string="Model", readonly=False)
+    year = fields.Char(string="Year", readonly=False)
+    rows = fields.Integer(string="Rows", readonly=False)
+    crops_being_planted = fields.Char(string="Crops being planted", readonly=False)  # many2many
+    acres_planted_per_year = fields.Char(string="Acres Planted Per Year", readonly=False)
+    spacing = fields.Integer(string="Spacing", readonly=False)
+    frame_type = fields.Char(string="Frame Type", readonly=False)  # many2one
+    seed_delivery = fields.Char(string="Seed Delivery", readonly=False)
+    original_planter_monitor = fields.Char(string="Original Planter Monitor", readonly=False)
+    current_planter_monitor = fields.Char(string="Current Planter Monitor", readonly=False)
+    meter_type = fields.Char(string="Meter Type", readonly=False)  # many2one
+    meter_drive_system = fields.Char(string="Meter Drive System", readonly=False)  # many2one
+    closing_system = fields.Char(string="Closing System", readonly=False)  # many2one
+    hopper_type = fields.Char(string="Hopper Type", readonly=False)  # many2one
+    row_config = fields.Char(string="Row Configuration", readonly=False)
+    no_of_regular_parallel_arms = fields.Integer(string="No of Regular Parallel Arms", readonly=False)
+    no_of_long_parallel_arms = fields.Integer(string="No of Long Parallel Arms", readonly=False)
+    downforce_system = fields.Char(string="Down Force System", readonly=False)
+    no_of_vr_motors = fields.Integer(string="No of VR Motors", readonly=False)
+    row_cleaner_make = fields.Char(string="Row Cleaner Make", readonly=False)
+    row_cleaner_model = fields.Char(string="Row Cleaner Model", readonly=False)
+    no_till_coulters = fields.Char(string="No-Till Coulters", readonly=False)
+    liquid_application_method_1 = fields.Char(string="Liquid Application Method #1", readonly=False)  # many2one
+    liquid_application_method_2 = fields.Char(string="Liquid Application Method #2", readonly=False)  # many2one
+    firmer = fields.Char(string="Firmer", readonly=False)
+    gps_make = fields.Char(string="GPS Make", readonly=False)
+    gps_monitor = fields.Char(string="GPS Monitor", readonly=False)
+    no_of_hydraulic_remotes = fields.Integer(string="No of Hydraulic Remotes", readonly=False)
+    hydraulic_capacity = fields.Char(string="Hydraulic Capacity", readonly=False)
+    width = fields.Float(string="Width", readonly=False)
+    monitor = fields.Char(string="Monitor", readonly=False)
+    mpn = fields.Char(string="MPN")
+    tax_agency = fields.Char(string="Tax Agency")
+    assets_account = fields.Many2one('account.account', string='Assets Account')
+    accumulated_depreciation = fields.Float(string='Accumulated Depreciation')
+    # serial_no = fields.Many2many('stock.production.lot', 'contact_product_serial_no', string='Serial No')
+    preferred_vendor = fields.Many2one('res.partner', string="Preferred Vendor")
+    # reorder_pt_min = fields.Float(string='Reorder Pt (Min)')
+    # reorder_pt_max = fields.Float(string='Max')
+    tax_status = fields.Selection([
+        ('tax', 'Tax'),
+        ('non', 'Non'),
+        ('yes', 'Yes'),
+    ], default='non', string="Tax Status")
+    serial_no = fields.Many2many('stock.production.lot', 'contact_product_serial_no', string='Serial No',
+                                 compute='_compute_serial_no', readonly=False)
 
-    @api.onchange('name_id')
-    def onchange_skus(self):
-        for record in self:
-            # print(record.id)
-            skus = self.env['product.template'].search(
-                [('id', '=', record.name_id.id), ('name', '=', record.name_id.name)], limit=1)
-            record.product_category_id = skus.categ_id.id
-            record.warranty_start_date = skus.warranty_start_date
-            record.warranty_end_date = skus.warranty_end_date
-            record.expiration_date = skus.expiration_date
-            record.company_id = skus.company_id
-            record.model = skus.model
-            record.year = skus.year
-            record.partner_id = record.id
-            record.serial_no = skus.serial_no
+
+    # @api.onchange('name_id')
+    # def onchange_skus(self):
+    #     for record in self:
+    #         # print(record.id)
+    #         skus = self.env['product.template'].search(
+    #             [('id', '=', record.name_id.id), ('name', '=', record.name_id.name)], limit=1)
+    #         record.product_category_id = skus.categ_id.id
+    #         record.warranty_start_date = skus.warranty_start_date
+    #         record.warranty_end_date = skus.warranty_end_date
+    #         record.expiration_date = skus.expiration_date
+    #         record.company_id = skus.company_id
+    #         record.model = skus.model
+    #         record.year = skus.year
+    #         record.partner_id = record.id
+    #         record.serial_no = skus.serial_no
 
 
 
